85d6f4f481b5c977cf91c9b94289f1ced196e664
MortezaFa <teezy10299@gmail.com>
2024-12-25 14:48:58 -0700

    small tcp server. For practice. Networking is cool. Merry christmas

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
+/* GetHeadInfo
+ */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Print("Hello world")
+
+	// port number
+	service := "10.0.0.74:1300"
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		location, err := time.LoadLocation("Asia/Tokyo")
+		checkError(err)
+		japanTime := time.Now().In(location).String()
+		conn.Write([]byte(fmt.Sprintf("%s\n", japanTime)))
+		conn.Close()
+	}
 }
